e77ad67c0ffa46513aa7e352c3d97ade98613fe6
Matthew Elliston <mpgelliston@gmail.com>
2023-07-21 09:09:55 +0100

    Added the Protobuf server logic

---
+++ sum/server/main.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/melliston/go-protobuf/sum/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.SumServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("unable to listen on %s: %v\n", addr, err)
+	}
+
+	log.Printf("listening on %s", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve %v\n", err)
+	}
+}
